c28f06951b35095b176da16405de116e8fcbe6c4
baiwumm <baiwumm@foxmail.com>
2024-04-01 16:32:32 +0800

    config: 配置 srcDir 目录，新增 stylelint 配置

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  // 源目录
+  srcDir: 'src/',
+  // 引入模块
+  modules: ['@nuxtjs/stylelint-module']
 })
