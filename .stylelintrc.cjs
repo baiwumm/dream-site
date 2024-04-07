6e75e1db276a5b73e703a2d6b1f5ee7fc3e70f95
baiwumm <baiwumm@foxmail.com>
2024-04-07 17:22:46 +0800

    feat: 安装 stylelint-config-recommended-vue ，修复 vue 文件中 stylelint 导致的报错

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['stylelint-config-standard'],
+  extends: ['stylelint-config-standard', 'stylelint-config-recommended-vue'],
   plugins: ['stylelint-order'],
   rules: {
     // 颜色指定大写
