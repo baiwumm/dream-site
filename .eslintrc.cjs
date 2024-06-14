4f8e2974f6fd672e687d04cea34775f94f24df13
baiwumm <baiwumm@foxmail.com>
2024-06-14 14:13:39 +0800

    ci: 关闭 eslint camelcase 属性检测规则

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,7 +13,7 @@ module.exports = {
   },
   plugins: ['vue'],
   rules: {
-    camelcase: 2, // 驼峰
+    camelcase: 'off', // 驼峰
     indent: [2, 2], // 缩进2个空格
     semi: [2, 'never'], // 要求或禁止使用分号代替 ASI,即禁用行尾使用分号
     quotes: ['error', 'single'], // 强制使用一致的反勾号、双引号或单引号
