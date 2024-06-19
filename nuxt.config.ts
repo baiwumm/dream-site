bcf81a998bf1ecb522179d7d3f6bc70bd8a1c66f
baiwumm <baiwumm@foxmail.com>
2024-06-19 08:50:26 +0800

    feat: 更改本地开发地址

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   runtimeConfig: {
     public: {
-      baseUrl: process.env.BASE_URL || 'http://localhost:3000'
+      baseUrl: process.env.BASE_URL || 'https://dream-site.cn'
     }
   },
   // SEO 和 Meta
