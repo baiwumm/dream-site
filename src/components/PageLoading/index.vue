03619510dfed9267e9cc16ba6b09b7818cd5cb08
baiwumm <baiwumm@foxmail.com>
2024-06-20 15:11:29 +0800

    perf: 优化代码结构

--- src/components/PageLoading/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div
-    class="relative flex w-full h-40 justify-center items-center flex-col z-[99] overflow-hidden bg-white dark:bg-slate-900"
-  >
-    <div
-      class="absolute w-12 h-12 rotate-[165deg] before:content-[''] after:content-[''] before:absolute after:absolute before:top-2/4 after:top-2/4 before:left-2/4 after:left-2/4 before:block after:block before:w-[.5em] after:w-[.5em] before:h-[.5em] after:h-[.5em] before:rounded after:rounded before:-translate-x-1/2 after:-translate-x-1/2 before:-translate-y-2/4 after:-translate-y-2/4 before:animate-[loaderBefore_2s_infinite] after:animate-[loaderAfter_2s_infinite]"
-    />
-  </div>
-</template>
