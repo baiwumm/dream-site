f68fde205e63a64b00e56eb207d4a364e3cd6289
白雾茫茫丶 <baiwumm@foxmail.com>
2025-07-16 16:16:51 +0800

    feat: 添加 SplashCursor 鼠标特效组件

--- src/app.vue
+++ src/app.vue
@@ -30,6 +30,23 @@ useHead({
       <NuxtLoadingIndicator />
       <NuxtPage />
     </NuxtLayout>
+    <!-- 鼠标特效 -->
+    <SplashCursor
+      :SIM_RESOLUTION="128"
+      :DYE_RESOLUTION="1440"
+      :CAPTURE_RESOLUTION="512"
+      :DENSITY_DISSIPATION="3.5"
+      :VELOCITY_DISSIPATION="2"
+      :PRESSURE="0.1"
+      :PRESSURE_ITERATIONS="20"
+      :CURL="3"
+      :SPLAT_RADIUS="0.2"
+      :SPLAT_FORCE="6000"
+      :SHADING="true"
+      :COLOR_UPDATE_SPEED="10"
+      :BACK_COLOR="{ r: 0.5, g: 0, b: 0 }"
+      :TRANSPARENT="true"
+    />
   </div>
 </template>
 
