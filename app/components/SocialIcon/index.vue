958edb9f658fca3d42e887f47d5e809bc1806f71
白雾茫茫丶 <baiwumm@foxmail.com>
2025-07-22 17:17:43 +0800

    feat: 修改社交图标不能跳转的问题

--- app/components/SocialIcon/index.vue
+++ app/components/SocialIcon/index.vue
@@ -2,7 +2,7 @@
  * @Author: 白雾茫茫丶<baiwumm.com>
  * @Date: 2024-05-30 13:54:54
  * @LastEditors: 白雾茫茫丶<baiwumm.com>
- * @LastEditTime: 2025-07-18 16:43:48
+ * @LastEditTime: 2025-07-22 17:17:13
  * @Description: 社交图标
 -->
 <script setup lang="ts">
@@ -13,6 +13,6 @@ defineProps<Social>();
 
 <template>
   <UTooltip :text="tip">
-    <UButton :icon="icon" size="lg" color="neutral" variant="ghost" class="cursor-pointer" />
+    <UButton :icon="icon" size="lg" color="neutral" variant="ghost" class="cursor-pointer" :to="url" target="_blank" />
   </UTooltip>
 </template>
