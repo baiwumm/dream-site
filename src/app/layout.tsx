5dc659e6ab340817dc460cc17acaa47dd0e83234
白雾茫茫丶 <baiwumm@foxmail.com>
2026-01-21 16:59:18 +0800

    feat: 安装 Shadcn UI 库

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import pkg from '#/package.json';
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,10 +14,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="zh-CN">
+      <meta name="version" content={pkg.version} />
+      <link rel="stylesheet" href="https://cdn.baiwumm.com/fonts/MapleMono-CN-Regular/result.css" />
+      <body>
         {children}
       </body>
     </html>
