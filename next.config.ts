8f30bed41fd98b5574f5ca1bd6a44af96dbcb5c1
白雾茫茫丶 <baiwumm@foxmail.com>
2026-01-29 14:58:08 +0800

    feat(config): 添加 Images 配置

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
-  }
+  },
+  images: {
+    remotePatterns: [new URL('https://api.baiwumm.com/**'), new URL('https://cdn.baiwumm.com/**')],
+  },
 };
 
 export default nextConfig;
