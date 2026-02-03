7feafa6da99ab871a59cc5f5e59d246b1a311fbd
白雾茫茫丶 <baiwumm@foxmail.com>
2026-02-03 14:42:14 +0800

    feat(NotFound): 添加 404 页面

---
+++ src/app/not-found.tsx
@@ -0,0 +1,27 @@
+/*
+ * @Author: 白雾茫茫丶<baiwumm.com>
+ * @Date: 2025-11-20 14:00:11
+ * @LastEditors: 白雾茫茫丶<baiwumm.com>
+ * @LastEditTime: 2026-02-03 14:41:02
+ * @Description: 404 页面
+ */
+import { Button } from "@heroui/react";
+import Link from 'next/link';
+import { type FC } from 'react';
+
+const NotFound: FC = () => {
+  return (
+    <div className="flex-1 flex justify-center items-center">
+      <div className="text-center">
+        <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance text-accent sm:text-7xl">404</h1>
+        <p className="mt-6 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">看来这个页面去环球旅行了，还没寄明信片回来。</p>
+        <div className="flex items-center justify-center mt-10">
+          <Link href="/">
+            <Button>回到首页</Button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default NotFound;
