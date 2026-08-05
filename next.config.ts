2a392239597c761c974009f7f44eb659d7413f6b
baiwumm <me@baiwumm.com>
2026-08-05 15:11:45 +0800

    build(next-config): 移除baiwumm图片远程域名配置

--- next.config.ts
+++ next.config.ts
@@ -6,8 +6,6 @@ const nextConfig: NextConfig = {
   },
   images: {
     remotePatterns: [
-      new URL('https://api.baiwumm.com/**'),
-      new URL('https://cdn.baiwumm.com/**'),
       new URL('https://athbiwlqrieaoetfapxd.supabase.co/**'),
     ],
     unoptimized: true, // 禁用 Vercel 图片优化
